55942b4bd8954fe2921dcc98e73a4889ae88b0e8
WENHAO TANG <wt215@ic.ac.uk>
2018-11-11 06:21:06 +0800

    1.0.2

--- R/bayNorm.r
+++ R/bayNorm.r
@@ -285,7 +285,9 @@ bayNorm <- function(
         } else {
             # non-UMI
             DataList_sr <- lapply(seq_along(Levels), function(x) {
-                round(Data[, which(Conditions == Levels[x])]/UMI_sffl[x])
+                tempp<-round(Data[, which(Conditions == Levels[x])]/UMI_sffl[x])
+                tempp<-tempp[-drop(which(rowMeans(tempp)==0)),]
+                return(tempp)
             })
         }
 
@@ -524,6 +526,7 @@ bayNorm_sup <- function(
             Data_sr <- Data
         } else {
             Data_sr <- round(Data/UMI_sffl)
+            Data_sr <- Data_sr[-which(rowMeans(Data_sr)==0),]
         }
 
         MU_input = PRIORS$MME_prior$MME_MU
@@ -582,7 +585,9 @@ bayNorm_sup <- function(
         } else {
             # non-UMI
             DataList_sr <- lapply(seq_along(Levels), function(x) {
-                round(Data[, which(Conditions == Levels[x])]/UMI_sffl[x])
+                tempp<-round(Data[, which(Conditions == Levels[x])]/UMI_sffl[x])
+                tempp<-tempp[-drop(which(rowMeans(tempp)==0)),]
+                return(tempp)
             })
         }
 
